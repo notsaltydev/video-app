6d71b50e164fffb84eaf52c13797fb7f716e8d49
adamnieslony <notsaltydev@gmail.com>
2017-11-14 22:53:59 +0100

    improvement styles, add mute option

--- src/app/application/video-player/video-player.component.ts
+++ src/app/application/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {VideoService} from '../../services/video.service';
 import {Video} from './model/video.interface';
 
@@ -9,66 +9,21 @@ import {Video} from './model/video.interface';
 })
 export class VideoPlayerComponent implements OnInit {
 
-  @ViewChild('videoPlayer') videoPlayer: any;
-  @ViewChild('progressBar') progressBar: any;
-  @ViewChild('volumeSlider') volumeSlider: any;
-  @ViewChild('allowFullscreen') allowFullscreen: any;
-  percentage: any;
+  @ViewChild('videoPlayer') videoPlayer: ElementRef;
+  @ViewChild('progressBar') progressBar: ElementRef;
+  @ViewChild('volumeSlider') volumeSlider: ElementRef;
+  @ViewChild('allowFullscreen') allowFullscreen: ElementRef;
+  percentage: string;
+  currentTime: number;
+  duration: number;
   currentVideo: Video;
-  private currentTime: any;
-  private duration: any = '0.00';
   videos: Array<Video> = [];
-  fullScreenEnabled: any = !!(document.fullscreenEnabled ||
-    document.webkitFullscreenEnabled ||
-    document.createElement('video').webkitRequestFullScreen);
-
 
   constructor(private _videoService: VideoService) {
+
   }
 
-  ngOnInit() {
-    this.videos = this._videoService.getAllVideos();
-    this.currentVideo = this.videos[0];
-    this.setEventListeners();
-  }
-
-  private setEventListeners(): void {
-    this.videoPlayer.nativeElement.load();
-    this.videoPlayer.nativeElement.removeAttribute('controls');
-    this.volumeSlider.nativeElement.value = this.videoPlayer.nativeElement.volume * 100;
-    this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
-      this.setDefaults();
-    });
-    this.videoPlayer.nativeElement.addEventListener('timeupdate', () => {
-      this.updateProgressBar();
-    });
-    this.volumeSlider.nativeElement.addEventListener('change', () => {
-      const volumeValue = this.volumeSlider.nativeElement.value / 100;
-      this.videoPlayer.nativeElement.volume = parseFloat(volumeValue.toString()).toFixed(1);
-    });
-    this.allowFullscreen.nativeElement.addEventListener('click', () => {
-      console.log('click');
-      this.handleFullscreen();
-    });
-  }
-
-  private setDefaults(): void {
-    this.duration = (this.videoPlayer.nativeElement.duration / 60).toFixed(2);
-    this.currentTime = (this.videoPlayer.nativeElement.currentTime).toFixed(2);
-  }
-
-  private updateProgressBar(): void {
-    this.duration = (this.videoPlayer.nativeElement.duration / 60).toFixed(2);
-    this.currentTime = (this.videoPlayer.nativeElement.currentTime / 60).toFixed(2);
-    this.percentage = ((100 / this.videoPlayer.nativeElement.duration) *
-      this.videoPlayer.nativeElement.currentTime).toFixed(2);
-  }
-
-  private setVolumeSlider(): void {
-    this.volumeSlider.nativeElement.value = this.videoPlayer.nativeElement.volume * 100;
-  }
-
-  onPlayOrPause(event: any): void {
+  onPlayOrPause(): void {
     this.videoPlayer.nativeElement.paused ? this.videoPlayer.nativeElement.play() : this.videoPlayer.nativeElement.pause();
   }
 
@@ -77,17 +32,78 @@ export class VideoPlayerComponent implements OnInit {
     this.videoPlayer.nativeElement.currentTime = 0;
   }
 
-  handleFullscreen() {
-    return document.documentElement.requestFullscreen();
-  }
-
-
-  setFullscreenData(state) {
-    this.videoPlayer.nativeElement.setAttribute('data-fullscreen', !!state);
-  }
-
   onChangeVideo(video: Video): void {
+    this.percentage = '0.00';
     this.currentVideo = video;
     this.setEventListeners();
   }
+
+  onMuteVideo() {
+    this.videoPlayer.nativeElement.muted = !this.videoPlayer.nativeElement.muted;
+  }
+
+  ngOnInit() {
+    this.onInitVideoPlayer();
+    this.fetchVideos();
+    this.setEventListeners();
+  }
+
+  private onInitVideoPlayer() {
+    this.videoPlayer.nativeElement.removeAttribute('controls');
+    this.volumeSlider.nativeElement.value = this.videoPlayer.nativeElement.volume * 100;
+  }
+
+  private fetchVideos(): void {
+    this.videos = this._videoService.getAllVideos();
+    this.currentVideo = this.videos[0];
+  }
+
+  private setEventListeners(): void {
+    this.videoPlayer.nativeElement.load();
+
+    this.videoPlayer.nativeElement.addEventListener('loadedmetadata', () => {
+      this.setDefaultsSettings();
+    });
+
+    this.videoPlayer.nativeElement.onended = () => {
+      const index = this.videos.indexOf(this.currentVideo);
+      if (index !== this.videos.length - 1) {
+        this.currentVideo = this.videos[index + 1];
+        this.setEventListeners();
+        this.onPlayOrPause();
+      } else {
+        this.currentVideo = this.videos[0];
+        this.setEventListeners();
+        this.percentage = '0.00';
+      }
+    };
+
+    this.videoPlayer.nativeElement.addEventListener('timeupdate', () => {
+      this.updateProgressBar();
+      this.updateCurrentTime();
+    });
+
+    this.volumeSlider.nativeElement.addEventListener('change', () => {
+      const volumeValue = this.volumeSlider.nativeElement.value / 100;
+      this.videoPlayer.nativeElement.volume = parseFloat(volumeValue.toString()).toFixed(1);
+    });
+  }
+
+  private setDefaultsSettings(): void {
+    this.currentTime = 0;
+    this.duration = (this.videoPlayer.nativeElement.duration / 60);
+  }
+
+  private updateProgressBar(): void {
+    this.percentage = ((100 / this.videoPlayer.nativeElement.duration) *
+      this.videoPlayer.nativeElement.currentTime).toFixed(2);
+  }
+
+  private updateCurrentTime(): void {
+    this.duration = (this.videoPlayer.nativeElement.duration / 60);
+    this.currentTime = (this.videoPlayer.nativeElement.currentTime / 60);
+  }
+
+
+
 }
