f2d32e06df791d9b4b72d73aa877369587d3b7a3
adamnieslony <notsaltydev@gmail.com>
2017-11-15 10:09:32 +0100

    add videos§

--- src/assets/mock-data.ts
+++ src/assets/mock-data.ts
@@ -3,21 +3,28 @@ export const mock_data = [
     title: 'Sample title #1',
     description: 'Lorem ipsum...',
     duration: '2.29',
-    cover: '../../../assets/image/default_placeholder.png',
+    cover: '../../../assets/image/video_test_img_1.png',
     src: '../../../assets/video/test_video_0.mp4',
   },
   {
     title: 'Sample title #2',
     description: 'Lorem ipsum...',
     duration: '2.29',
-    cover: '../../../assets/image/default_placeholder.png',
+    cover: '../../../assets/image/video_test_img_2.png',
     src: '../../../assets/video/test_video_1.mp4',
   },
   {
     title: 'Sample title #3',
     description: 'Lorem ipsum...',
     duration: '2.29',
-    cover: '../../../assets/image/default_placeholder.png',
+    cover: '../../../assets/image/video_test_img_3.png',
     src: '../../../assets/video/test_video_2.mp4',
+  },
+  {
+    title: 'Sample title #4',
+    description: 'Lorem ipsum...',
+    duration: '2.29',
+    cover: '../../../assets/image/video_test_img_4.png',
+    src: '../../../assets/video/test_video_3.mp4',
   }
 ];
